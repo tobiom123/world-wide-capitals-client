d617b22b4a07f3d87d055034ed6faa588e31b177
Oluwatobi Omotosho <tobiom@Oluwatobis-Mac-Studio.local>
2023-07-26 22:36:04 +0100

    Remove non-existence route

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -75,7 +75,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         axios
             .post('/reset-password', { token: router.query.token, ...props })
             .then(response =>
-                router.push('/login?reset=' + btoa(response.data.status)),
+                router.push('/?reset=' + btoa(response.data.status)),
             )
             .catch(error => {
                 if (error.response.status !== 422) throw error
@@ -95,7 +95,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             await axios.post('/logout').then(() => mutate())
         }
 
-        window.location.pathname = '/login'
+        window.location.pathname = '/'
     }
 
     useEffect(() => {
